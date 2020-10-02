03f72334321e533c58c757e3c492008210a318f4
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-02 18:07:43 -0400

    add moment for time format

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const path = require ('path')
-const members = require('./Members')
+const path = require('path');
+const moment = require('moment');
+const members = require('./Members');
 
 const app = express();
 //Middleware
-const logger =(req, res, next) => {
-
-    console.log('Hello')
-    next();
-}
+const logger = (req, res, next) => {
+	console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+	next();
+};
 
 //Init middleware
 app.use(logger);
 
-
-
-
 // Route to all members
-app.get('/api/members', (req, res)=>res.json(members));
+app.get('/api/members', (req, res) => res.json(members));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
