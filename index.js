d7269135d30c2481b19dc4e966e179cac5f6d4e3
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-02 18:01:05 -0400

    Add Middleware

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const path = require ('path')
+const members = require('./Members')
 
 const app = express();
+//Middleware
+const logger =(req, res, next) => {
 
-const members = require('./Members')
+    console.log('Hello')
+    next();
+}
+
+//Init middleware
+app.use(logger);
+
+
+
 
 // Route to all members
 app.get('/api/members', (req, res)=>res.json(members));
