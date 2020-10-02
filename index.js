5d1ab29152142b29e5c236026d703699dbc9688d
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-02 17:25:01 -0400

    add html

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require ('path')
 
 const app = express();
 
 app.get('/', (req, res)=>{
 
-res.send('<h1>Hello World!!<h1>');
+res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 })
 
