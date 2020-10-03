254836dab0755654f05a13c813f8055d007f9274
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-02 23:15:48 -0400

    express-handlebars added, delete, post, added.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -9,12 +10,22 @@ const app = express();
 //Init middleware
 //app.use(logger);
 
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Homepage Route
+app.get('/', (req, res) => res.prependListener('index'));
+
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Members API Route
 app.use('/api/members', require('./routes/api/members'));
