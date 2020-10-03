4b84399d958aac0a571888ffe114d115e8876bbf
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-02 22:03:06 -0400

    Built Api Members

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -10,28 +9,12 @@ const app = express();
 //Init middleware
 //app.use(logger);
 
-// Route to all members
-app.get('/api/members', (req, res) => res.json(members));
-
-// Get Single Member
-app.get('/api/members/:id', (req, res) => {
-
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-    if(found){
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    }
-    else{
-
-        res.status(400).json({msg: `No member with the id of ${req.params.id}`})
-    }
-    
-
-
-});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Route
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
