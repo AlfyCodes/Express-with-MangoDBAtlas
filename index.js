69b4be2c0045fc93e215834810e01efe92b1e489
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-03 00:12:23 -0400

    Member App Page running able to add, working on redirect

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./members');
 
 const app = express();
 //Middleware
@@ -14,18 +15,18 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars')
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Homepage Route
-app.get('/', (req, res) => res.prependListener('index'));
-
-
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API Route
 app.use('/api/members', require('./routes/api/members'));
--- routes/api/members.js
+++ routes/api/members.js
@@ -31,15 +31,17 @@ router.post('/', (req, res)=>{
         name: req.body.name,
         email: req.body.email,
         status: 'active'
-    }
+    };
 
     if(!newMember.name || !newMember.email){
         return res.status(400).json({msg: 'Please include a name and email'});
-    }
+    };
 
     members.push(newMember);
 
-    res.json(members);
+    //res.json(members); //Won't do this in real world instead
+
+    res.redirect('/');
 
 });
 
